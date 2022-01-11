a17a4a9577e7dbf4bde253f762283e464f1b0183
louiscklaw <louiscklaw@gmail.com>
2022-01-12 00:28:35 +0800

    update,

--- jenkins-helloworld/init_worker.sh
+++ jenkins-helloworld/init_worker.sh
@@ -4,4 +4,16 @@ set -ex
 
 echo 'helloworld init_worker'
 
-https://raw.githubusercontent.com/louiscklaw/jenkins-playlist/master/jenkins-helloworld/init_worker.sh
+sudo apt install -y curl wget git
+
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt install -y yarn
+
+sudo apt remove --autoremove --purge python3-pipenv || true
+sudo apt remove --autoremove --purge pipenv || true
+sudo pip3 install pipenv
