4955c12976b2edb8d5f8d31b251f7d8edc1c5517
louiscklaw <louiscklaw@gmail.com>
2022-01-11 20:38:22 +0800

    update,

--- jenkins-helloworld/up.sh
+++ jenkins-helloworld/up.sh
@@ -2,22 +2,22 @@
 
 set -ex
 
-docker network create jenkins || true
+# docker network create jenkins || true
 
 
-docker run \
-  --name jenkins-docker \
-  --rm \
-  --detach \
-  --privileged \
-  --network jenkins \
-  --network-alias docker \
-  --env DOCKER_TLS_CERTDIR=/certs \
-  --volume jenkins-docker-certs:/certs/client \
-  --volume jenkins-data:/var/jenkins_home \
-  --publish 2376:2376 \
-  docker:dind \
-  --storage-driver overlay2
+# docker run \
+#  --name jenkins-docker \
+#  --rm \
+#  --detach \
+#  --privileged \
+#  --network jenkins \
+#  --network-alias docker \
+#  --env DOCKER_TLS_CERTDIR=/certs \
+#  --volume jenkins-docker-certs:/certs/client \
+#  --volume jenkins-data:/var/jenkins_home \
+#  --publish 2376:2376 \
+#  docker:dind \
+#  --storage-driver overlay2
 
 docker run \
   --name jenkins-blueocean \
@@ -29,6 +29,7 @@ docker run \
   --env DOCKER_TLS_VERIFY=1 \
   --publish 8080:8080 \
   --publish 50000:50000 \
+  --publish 51100:8080 \
   --volume jenkins-data:/var/jenkins_home \
   --volume jenkins-docker-certs:/certs/client:ro \
   myjenkins-blueocean:1.1
